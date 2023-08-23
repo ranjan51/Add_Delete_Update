49b30453f7d4a543c39f3e81669458be2a96309c
Ranjan Kishor <ranjankishor83@gmail.com>
2023-08-23 16:56:12 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,66 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const[user,setUser]  = useState({
-    first:"",
-    last:""
-  })
+  const[user,setUser]  = useState<any>([])
+  const[firstname, setFirstname] = useState<any>()
+  const[lastname, setLastname] = useState<any>()
+
+  const [editname,setEditname] = useState<any>("")
   
   // const[user,setUser]  = useState<any[]>([])
- const handleChange = (event:any)=>{
-  console.log(event.target);
-    setUser({...user,[event.target.name]:event.target.value})
-   console.log(`first:${user.first}---last:${user.last} `)
+  const submit =()=>{
+   const newuser={
+    fname:firstname,
+    id:Date.now(),
+    isEdit:false
+   }
+   setUser([...user,newuser])
+   setFirstname("")
+  }
+
+  const del = (id:any)=>{
+   setUser(user.filter((X:any)=>X.id !== id))
+  }
+
+
+
+  const sub = (id:any) =>{
+    setUser(user.map((x:any) => x.id === id ? {...x,fname:editname,isEdit:false}:x))
+
+  }
   
- }
   return (
-    <div>
-   <input type="text" onChange={handleChange} name="first" />
-   <input type="text" onChange={handleChange} name="last" />
+    <div>     
+   <input type="text" name="first" onChange={(e:any)=>setFirstname(e.target.value)} value={firstname} />
+   {/* <input type="text" name="last" /> */}
    {/* <input type="text" /> */}
+   <button onClick={submit} >submit</button>
+   
+  {user.map((X:any)=>{
+      const updatebtn = (id:any) => {
+        setEditname(X.firstname)
+        setUser(user.map((X:any) => X.id === id ? {...X,isEdit:true}:X))
+       }
+    return  (
+      <>
+      <h1>{X.fname}</h1>
+      <button onClick={() =>del(X.id)}>delete</button>
+      <button onClick={() => updatebtn(X.id)}>Update</button>
+  
+    {X.isEdit ?
+    <div>
+      <input placeholder="update" name={editname} onChange={(e) => setEditname(e.target.value)}/>
+      <button onClick={() => sub(X.id)}>submit</button>
+      </div> :
+       null
+      }
+      
+
+      </>
+    ) 
+   
+  })}
+
 
    </div>
   );
